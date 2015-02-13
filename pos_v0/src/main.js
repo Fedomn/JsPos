27bb4e20195a8e40f32466eeef233647e573cbb6
Fedomn <shadowforfiend@gmail.com>
2015-02-13 17:54:07 +0800

    inline printresult

--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -2,16 +2,15 @@ function printInventory(inputs) {
     var inventorySumPrice = 0;
     var inventoryPrintResult = '';
 
-    for (var item in inputs) {
+    for (var item = 0; item < inputs.length; item++) {
         var itemSumPrice = inputs[item].count * inputs[item].price;
         inventorySumPrice += itemSumPrice;
 
-        var itemPrintResult = '名称:' + inputs[item].name +
-            ',数量:' + inputs[item].count + inputs[item].unit +
-            ',单价:' + inputs[item].price +
-            '.00(元),小计:' +
-            itemSumPrice + '.00(元)\n';
-        inventoryPrintResult += itemPrintResult;
+        inventoryPrintResult += '名称:' + inputs[item].name +
+                                ',数量:' + inputs[item].count + inputs[item].unit +
+                                ',单价:' + inputs[item].price +
+                                '.00(元),小计:' +
+                                itemSumPrice + '.00(元)\n';
     }
 
     inventoryPrintResult =  '***<没钱赚商店>购物清单***\n' +
