7e639aaaddc10673cc10c4749a31980984899da3
Fedomn <shadowforfiend@gmail.com>
2015-02-13 17:50:19 +0800

    done pos_v0.1

--- pos_v0.1/spec/main-spec.js
+++ pos_v0.1/spec/main-spec.js
@@ -3,29 +3,58 @@ describe('pos_v0.1', function () {
     var inputs;
 
     beforeEach(function () {
-        inputs = [
-            {
-                barcode: 'ITEM000000',
-                name: '可口可乐',
-                unit: '瓶',
-                price: 3.00,
-                count: 5
-            },
-            {
-                barcode: 'ITEM000001',
-                name: '雪碧',
-                unit: '瓶',
-                price: 3.00,
-                count: 2
-            },
-            {
-                barcode: 'ITEM000004',
-                name: '电池',
-                unit: '个',
-                price: 2.00,
-                count: 1
-            }
-        ];
+        inputs =
+            [
+                {
+                    barcode: 'ITEM000000',
+                    name: '可口可乐',
+                    unit: '瓶',
+                    price: 3.00
+
+                },
+                {
+                    barcode: 'ITEM000000',
+                    name: '可口可乐',
+                    unit: '瓶',
+                    price: 3.00
+                },
+                {
+                    barcode: 'ITEM000000',
+                    name: '可口可乐',
+                    unit: '瓶',
+                    price: 3.00
+                },
+                {
+                    barcode: 'ITEM000000',
+                    name: '可口可乐',
+                    unit: '瓶',
+                    price: 3.00
+                },
+                {
+                    barcode: 'ITEM000000',
+                    name: '可口可乐',
+                    unit: '瓶',
+                    price: 3.00
+                },
+                {
+                    barcode: 'ITEM000001',
+                    name: '雪碧',
+                    unit: '瓶',
+                    price: 3.00
+                },
+                {
+                    barcode: 'ITEM000001',
+                    name: '雪碧',
+                    unit: '瓶',
+                    price: 3.00
+                },
+                {
+                    barcode: 'ITEM000004',
+                    name: '电池',
+                    unit: '个',
+                    price: 2.00
+                }
+            ]
     });
 
     it('should print correct text', function () {
--- pos_v0.1/src/main.js
+++ pos_v0.1/src/main.js
@@ -1,4 +1,35 @@
 function printInventory(inputs) {
+    var inventorySumPrice = 0;
+    var inventoryPrintResult = '';
 
+    var groupResult = _.groupBy(inputs, 'name');
+    var mapResult = _.map(groupResult, function (value, key) {
+        return {
+            'name': key,
+            'barcode': value[0].barcode,
+            'unit': value[0].unit,
+            'price': value[0].price,
+            'count': value.length
+        };
+    });
 
+    for (var item = 0; item < mapResult.length; item++) {
+        var itemSumPrice = mapResult[item].count * mapResult[item].price;
+        inventorySumPrice += itemSumPrice;
+
+        inventoryPrintResult += '名称:' + mapResult[item].name +
+                                ',数量:' + mapResult[item].count + mapResult[item].unit +
+                                ',单价:' + mapResult[item].price +
+                                '.00(元),小计:' +
+                                itemSumPrice + '.00(元)\n';
+    }
+
+    inventoryPrintResult = '***<没钱赚商店>购物清单***\n' +
+                            inventoryPrintResult +
+                            '----------------------\n' +
+                            '总计:' + inventorySumPrice + '.00(元)\n' +
+                            '**********************';
+
+    console.info(inventoryPrintResult);
+    console.log(inventoryPrintResult);
 }
